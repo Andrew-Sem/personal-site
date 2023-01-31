a4b41c248cfdca2ee8175f59d3bf2e50dd94b8fc
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-31 14:07:13 +0300

    add link text hover duration

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,7 +8,7 @@ interface LinkProps{
 export const Link: FC<LinkProps> = ({href, children}) => {
     return (
         <a
-            className={"relative inline-block text-xl hover:text-white before:block before:absolute before:-inset-x-0.5 before:bottom-0 before:bg-indigo-500 before:h-1.5 hover:before:h-full hover:before:bottom-0 before:duration-200 cursor-pointer"}
+            className={"relative inline-block text-xl hover:text-white duration-200 before:block before:absolute before:-inset-x-0.5 before:bottom-0 before:bg-indigo-500 before:h-1.5 hover:before:h-full hover:before:bottom-0 before:duration-200 cursor-pointer hover:duration-400"}
             href={href}
         >
             <span className={"relative"}>{children}</span>
