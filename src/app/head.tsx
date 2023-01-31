b64cd02d6b1c83b555b9099a549ebb1272a4288b
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-31 16:51:03 +0300

    fix typo: Semenov -> Semyonov

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Andrew Semenov</title>
+      <title>Andrew Semyonov</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Andrew Semenov personal website" />
+      <meta name="description" content="Andrew Semyonov personal website" />
     </>
   )
 }
