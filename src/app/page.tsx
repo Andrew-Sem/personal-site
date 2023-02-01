7e55ab20c4dbef17b907bc493895a6cc9bb0dc48
Andrew-Sem <avvyayvayva@gmail.com>
2023-02-01 14:01:50 +0300

    fix resume downloading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,51 @@
-import {Link} from "@/components/Link";
-import {P} from "@/components/P";
+import { Link } from "@/components/Link";
+import { P } from "@/components/P";
 
 export default function Home() {
-    return (
-        <main className={"container mx-auto px-4"}>
-            <div className={"lg:w-3/5"}>
-                <h1 className={"text-7xl mt-10 font-semibold"}>Hello</h1>
-                <h3 className={"text-3xl mt-5 "}>My name is Andrew Semyonov</h3>
-                <P>
-                    I am frontend developer, currently located in Volzhskiy, Russia.<br/>
-                    You can find my projects on&nbsp;
-                    <Link href={"https://github.com/Andrew-Sem"}>GitHub</Link>
-                </P>
-                <P>
-                    Also take a look at my collaboration with backend developer&nbsp;
-                    <Link href={"https://github.com/SergeyPodgorny"}>Sergey Podgorny</Link>
-                    &nbsp;-&nbsp;
-                    <Link href={"https://github.com/Friendly-neighborhood-development/Fnd_games_store"}>
-                        Games Store
-                    </Link>
-                </P>
-                <div className={"mt-5 flex space-x-2 items-center"}>
-                    <h4 className={"text-2xl"}>Resume</h4>
-                    <a href='../../public/Resume.pdf' download={'losos.pdf'}>Download</a>
-                </div>
-                <div className={"mt-5"}>
-                    <h4 className={"text-2xl"}>Contact me</h4>
-                    <ul className={"space-y-1"}>
-                        <li>
-                            <Link href={"http://t.me/keax4208"}>Telegram</Link><br/>
-                        </li>
-                        <li>
-                            <Link href={"mailto:andrew.semyonov02@gmail.com"}>Email</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main className={"container mx-auto px-4"}>
+      <div className={"lg:w-3/5"}>
+        <h1 className={"text-7xl mt-10 font-semibold"}>Hello</h1>
+        <h3 className={"text-3xl mt-5 "}>My name is Andrew Semyonov</h3>
+        <P>
+          I am frontend developer, currently located in Volzhskiy, Russia.
+          <br />
+          You can find my projects on&nbsp;
+          <Link href={"https://github.com/Andrew-Sem"}>GitHub</Link>
+        </P>
+        <P>
+          Also take a look at my collaboration with backend developer&nbsp;
+          <Link href={"https://github.com/SergeyPodgorny"}>
+            Sergey Podgorny
+          </Link>
+          &nbsp;-&nbsp;
+          <Link
+            href={
+              "https://github.com/Friendly-neighborhood-development/Fnd_games_store"
+            }
+          >
+            Games Store
+          </Link>
+        </P>
+        <div className={"mt-5 flex space-x-2 items-center"}>
+          <h4 className={"text-2xl"}>Resume</h4>
+          <a href="/pdfs/resume.pdf" download={"resume.pdf"}>
+            Download
+          </a>
+        </div>
+        <div className={"mt-5"}>
+          <h4 className={"text-2xl"}>Contact me</h4>
+          <ul className={"space-y-1"}>
+            <li>
+              <Link href={"http://t.me/keax4208"}>Telegram</Link>
+              <br />
+            </li>
+            <li>
+              <Link href={"mailto:andrew.semyonov02@gmail.com"}>Email</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
